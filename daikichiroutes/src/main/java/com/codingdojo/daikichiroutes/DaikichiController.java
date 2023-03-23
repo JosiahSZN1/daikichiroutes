d00a6a47a73c914d911f98b79b0dfcfd34f48f3d
Levi Josiah Mariano <lejosiahm@gmail.com>
2023-03-22 21:39:29 -0700

    daikichi path variables added

--- daikichiroutes/src/main/java/com/codingdojo/daikichiroutes/DaikichiController.java
+++ daikichiroutes/src/main/java/com/codingdojo/daikichiroutes/DaikichiController.java
@@ -2,6 +2,7 @@
 
 package com.codingdojo.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,4 +32,27 @@ public class DaikichiController {
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
+	
+//	Daikichi Path Variables tasks:
+	
+//	Add 2 new routes with corresponding method definitions that will accept path variables.
+	
+//	Have an http GET request to 'http://localhost:8080/daikichi/travel/kyoto' where the url will take any string for the city, and display text that says for example, 'Congratulations! You will soon travel to kyoto!'.
+	
+	@RequestMapping("/travel/{place}")
+	public String travel(@PathVariable("place") String place) {
+		return "Congratulations! You will soon travel to " + place + "!";
+	}
+	
+//	Have an http GET request to 'http://localhost:8080/daikichi/lotto/8', where the url will take any integer. If the number is even, display text that says 'You will take a grand journey in the near future, but be weary of tempting offers'. If it is odd, display text that says "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends."
+	
+	@RequestMapping("/lotto/{number}")
+	public String lotto(@PathVariable("number") int guess) {
+		if(guess % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		} else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
+	}
+	
 }
